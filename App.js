067623aab72b7d5df94f1c55c94e1f930f2e9cba
rs.rupesh95 <rs.rupesh95@gmail.com>
2020-05-04 23:24:10 +0530

    login mutation added

--- App.js
+++ App.js
@@ -1,38 +1,54 @@
-import React , {useState , useEffect} from "react";
-import { AppLoading } from 'expo';
-import { Container, Text } from 'native-base';
-import * as Font from 'expo-font';
-import { Ionicons } from '@expo/vector-icons';
+import React, { useState, useEffect } from "react";
+import { AppLoading } from "expo";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import SignupScreen from "./Screens/SignupScreen";
 import LoginScreen from "./Screens/LoginScreen";
 
+import { AppRegistry } from "react-native";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: 'https://salty-thicket-40672.herokuapp.com/graphql'
+});
+
+const client = new ApolloClient({
+  cache,
+  link
+});
 
 const Stack = createStackNavigator();
 export default function App() {
-    const [isReady , setIsReady] = useState(false);
- 
-    useEffect( async() => {
-      await Font.loadAsync({
-        Roboto: require('native-base/Fonts/Roboto.ttf'),
-        Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
-        ...Ionicons.font,
-      });
-      setIsReady(true);
-    } , []);
+    const [isReady, setIsReady] = useState(false);
+
+    useEffect(async () => {
+        await Font.loadAsync({
+            Roboto: require("native-base/Fonts/Roboto.ttf"),
+            Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+            ...Ionicons.font
+        });
+        setIsReady(true);
+    }, []);
 
     if (!isReady) {
-      return <AppLoading />;
+        return <AppLoading />;
     }
 
     return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="Login">
-                <Stack.Screen name="Login" component={LoginScreen} />
-                <Stack.Screen name="Signup" component={SignupScreen} />
-            </Stack.Navigator>
-        </NavigationContainer>
+        <ApolloProvider client={client}>
+            <NavigationContainer>
+                <Stack.Navigator initialRouteName="Login">
+                    <Stack.Screen name="Login" component={LoginScreen} />
+                    <Stack.Screen name="Signup" component={SignupScreen} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </ApolloProvider>
     );
 }
--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
     Text,
     Container,
@@ -8,25 +8,77 @@ import {
     Item,
     Input
 } from "native-base";
+import gql from "graphql-tag";
+import { useMutation } from "@apollo/react-hooks";
 
-const LoginScreen = ({navigation}) => {
+const Login = gql`
+    mutation login($email: String!, $password: String!) {
+        login(data: { email: $email, password: $password }) {
+            token
+            user {
+                name
+                email
+            }
+        }
+    }
+`;
+
+const LoginScreen = ({ navigation }) => {
+    const [email, setEmail] = useState("abc@gmail.com");
+    const [password, setPassword] = useState("");
+
+    const [login, { data, loading , error }] = useMutation(Login);
+
+    const loginHandeler = () => {
+        login({
+            variables: {
+                email,
+                password
+            }
+        });
+    };
+
+    if (loading) {
+        return <Text>Loading+</Text>;
+    }
+
+    if (error) {
+        return <Text>error+</Text>;
+    }
+
     return (
         <Container>
-            <Container style={{marginHorizontal: 10}}>
+            <Container style={{ marginHorizontal: 10 }}>
                 <Content>
                     <Form>
                         <Item>
-                            <Input placeholder="Username" />
+                            <Input
+                                onChangeText={text => setEmail(text)}
+                                placeholder="Username"
+                            />
                         </Item>
                         <Item last>
-                            <Input placeholder="Password" />
+                            <Input
+                                onChangeText={text => setPassword(text)}
+                                placeholder="Username"
+                                placeholder="Password"
+                            />
                         </Item>
-                        <Button  style={{marginVertical: 10}} block>
+                        <Button
+                            onPress={() => loginHandeler}
+                            style={{ marginVertical: 10 }}
+                            block
+                        >
                             <Text> Login </Text>
                         </Button>
-                        <Button block style={{marginVertical: 10}} onPress={() => navigation.navigate("Signup")}>
+                        <Button
+                            block
+                            style={{ marginVertical: 10 }}
+                            onPress={() => navigation.navigate("Signup")}
+                        >
                             <Text> Go to Sign Up </Text>
                         </Button>
+                        <Text>{data && data.login.user.name}</Text>
                     </Form>
                 </Content>
             </Container>
